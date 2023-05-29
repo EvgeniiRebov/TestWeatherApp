48399a3abcf2660cd73020e6641d850494ebf5ef
Evgenii Rebov <evgenii96rebov@gmail.com>
2023-05-29 18:19:14 +0300

    Rename controller

--- WeatherApp/WeatherViewController.swift
+++ WeatherApp/WeatherViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  WeatherViewController.swift
 //  WeatherApp
 //
 //  Created by Evgenii Rebov on 25.05.2023.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class WeatherViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
