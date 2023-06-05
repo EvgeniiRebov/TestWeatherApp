37cdad2e5df34eeeda05e2cf7823b0930413816d
Evgenii Rebov <evgenii96rebov@gmail.com>
2023-06-05 23:51:30 +0300

    Deleted `isRunningUnitTests`

--- WeatherApp/SceneDelegate.swift
+++ WeatherApp/SceneDelegate.swift
@@ -8,14 +8,6 @@
 import UIKit
 import CoreData
 
-func isRunningUnitTests() -> Bool {
-    let env = ProcessInfo.processInfo.environment
-    if env["XCTestConfigurationFilePath"] != nil {
-        return true
-    }
-    return false
-}
-
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
@@ -23,22 +15,20 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-        if !isRunningUnitTests() {
-            do {
-                let storeDirectory = NSPersistentContainer.defaultDirectoryURL()
-                let url = storeDirectory.appendingPathComponent("WeatherApp.sqlite")
-                let store = try CoreDataWeatherStore(storeURL: url)
-                let loader = LocalHitstoryLoader(store: store)
-
-                let nc = UINavigationController(rootViewController: WeatherComposer.compose(localLoader: loader,
-                                                                                            locationManager: LocationManager()))
-                nc.view.backgroundColor = .white
-                window?.rootViewController = nc
-                window?.makeKeyAndVisible()
-            } catch {
-                print("!!!", error)
-                fatalError()
-            }
+        do {
+            let storeDirectory = NSPersistentContainer.defaultDirectoryURL()
+            let url = storeDirectory.appendingPathComponent("WeatherApp.sqlite")
+            let store = try CoreDataWeatherStore(storeURL: url)
+            let loader = LocalHitstoryLoader(store: store)
+            
+            let nc = UINavigationController(rootViewController: WeatherComposer.compose(localLoader: loader,
+                                                                                        locationManager: LocationManager()))
+            nc.view.backgroundColor = .white
+            window?.rootViewController = nc
+            window?.makeKeyAndVisible()
+        } catch {
+            print("!!!", error)
+            fatalError()
         }
     }
 }
